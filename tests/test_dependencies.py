cae637c11ed3777452b9a5ddbcae63e0e0f3a3dc
limorl <limorl@microsoft.com>
2018-04-26 13:09:25 -0700

    refactor

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,16 +1,24 @@
 import pytest
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import helm
 import dependencies 
+import logging
+
+repo_name = "charts"
+repo_url = "https://github.com/kubernetes/charts.git"
+folders = ["stable", "incubator"]
 
 def test_match_charts():
         src_paths = ['./tests/data/src_pg.js', './tests/data/src_mongodb.js','./tests/data/src_mysql.js']
 
-        charts = dependencies.Dependencies().match_charts(src_paths)
+        helm_charts = helm.Helm(repo_name, repo_url, folders, logging.getLogger())
+        charts = helm_charts.get_helm_charts_details()
+        charts = dependencies.Dependencies().match_charts(charts, src_paths)
         
         assert charts
         assert len(charts) == 3
-
-        assert charts[0]['chart'] == 'https://github.com/kubernetes/charts/blob/master/stable/postgresql/Chart.yaml'
-        assert charts[1]['chart'] == 'https://github.com/kubernetes/charts/blob/master/stable/mongodb/Chart.yaml'
-        assert charts[2]['chart'] == 'https://github.com/kubernetes/charts/tree/master/stable/mysql/Chart.yaml'
+        #assert 'postgres' in charts[0][0]['name']
         
 test_match_charts()
