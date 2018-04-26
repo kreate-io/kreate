4ce9f4157a006caa09041afb15e414580c76e7ac
Limor Lahiani <limorl@Limors-MacBook-Pro.local>
2018-04-26 13:03:54 -0700

    refactor

--- model_scorer.py
+++ model_scorer.py
@@ -1,44 +1,15 @@
 from sklearn.externals import joblib
-import jellyfish
 import numpy as np
 
 class Scorer():
     DEPENDENCY_MODEL_PATH = './models/dependency_model.pkl'
-    JARO_WINKLER_THRESHOLD = 0.7
-    STOP_THRESHOLD = 0.9
 
     def __init__(self):
         self.__load_dependency_model()
 
+
     def __load_dependency_model(self):
         self.dependency_model= joblib.load(self.DEPENDENCY_MODEL_PATH)
-
-    def predict_charts(self, charts, features):
-        chart_scores = dict()
-
-        for chart in charts:
-            closest_distance = 0
-
-            keywords = []
-            keywords.append(chart['name'])
-
-            if 'keywords' in chart:
-                keywords = keywords + chart['keywords']
-
-            for feature in features:
-                for keyword in keywords:
-                    distance = jellyfish.jaro_winkler(feature, keyword)
-
-                    if distance > self.JARO_WINKLER_THRESHOLD and distance > closest_distance:
-                        closest_distance = distance
-
-            if closest_distance > 0:
-                chart_scores[chart['name']] = closest_distance
-
-            if closest_distance >= self.STOP_THRESHOLD:
-                break 
-        
-        return chart_scores
 
 
     def predict_dependencies(self, code_lines):
