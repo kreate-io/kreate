734c313e97366b9648273b4751442ce6a5fd40aa
torosent <torosent@microsoft.com>
2018-04-26 22:38:01 +0300

    initial version of pypi

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
 setup(
     name='Kreate',
     version='0.0.1',
     author='CSE Kings',
     author_email='torosent@microsoft.com',
-    packages=['kreate', 'kreate.test'],
-    scripts=['cli.py'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
     url='http://pypi.python.org/pypi/Kreate/',
-    license='LICENSE.txt',
     description='Fast deployment of dependencies to Kubernetes',
     long_description=open('README.md').read(),
-    install_requires=[
-        "gitpython",
-        "knack",
-        "pytest",
-        "nltk",
-        "sklearn",
-        "scipy",
-        "jellyfish"
-    ],
+    install_requires=["gitpython","knack","nltk","sklearn","scipy","jellyfish"],
+    extras_require={  # Optional
+        'test': ['pytest'],
+    }
+    #data_files=[('my_data', ['data/data_file'])]
 )
