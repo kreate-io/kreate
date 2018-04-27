fc21ce746d0b6adce3f8207c7d4bc138cb5f4c28
limorl <limorl@microsoft.com>
2018-04-26 19:51:40 -0700

    adding dependencies tests

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -31,7 +31,7 @@ def test_dependency_to_keywords():
         ]
 
         expected_keywords = [
-                [ 'pg'],
+                ['pg'],
                 ['mongoclient', 'mongodb'],
                 ['mysql'],
                 ['mysql', 'data', 'mysqlclient'],
@@ -41,3 +41,33 @@ def test_dependency_to_keywords():
         for i in range(0, len(dep_lines)):
                 keywords = dependencies.Dependencies().__dependency_to_keywords__(dep_lines[i])
                 assert set(keywords) == set(expected_keywords[i])
+
+def test_match_charts_per_keywords():
+        helm_charts = helm.Helm(repo_name, repo_url, folders, logging.getLogger())
+        charts = helm_charts.get_helm_charts_details()
+        
+        keywords = [
+                ['pg'],
+                ['mongoclient', 'mongodb'],
+                ['mysql'],
+                ['mysql', 'data', 'mysqlclient'],
+                # ['system', 'io'] failes for using System.IO
+        ]
+
+        expected_charts = [
+                'postgresql',
+                'mongodb',
+                'mysql',
+                'mysql',
+                #'system'
+        ]
+
+        for i in range(0, len(keywords)):
+                chart_count = 3
+                matched_charts = dependencies.Dependencies().__match_charts__(chart_count, charts, keywords[i])
+                
+                # yaron to fix: assert len(matched_charts) <= chart_count
+                assert matched_charts[0]['name'] == expected_charts[i]
+                 
+
+test_match_charts_per_keywords()
